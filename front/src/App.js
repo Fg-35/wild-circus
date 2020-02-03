772f65b68e9770ace8e56612c0eb92ad0ea7cd7a
Fg-35 <fabiengiboire17@gmail.com>
2020-02-03 20:33:13 +0100

    Navbar faite

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLogged: false,
+		}
+	}
+	userLogin = () => {
+		this.setState({ isLogged: true }); 
+	}
+
+	render() {
+		const { isLogged } = this.state;
+		return (
+			<div className="App bgcolor4">
+				<Navbar></Navbar>
+				{/* <Switch>
+					<Route path="/green" component={() => <Green isLogged={this.state.isLogged} />} />
+					<Route path="/pink" component={() => <Pink isLogged={this.state.isLogged} />} />
+					<Route path="/blue" component={() => <Blue isLogged={this.state.isLogged} />} />
+					<Route exact path="/" component={() => <Home login={this.userLogin} isLogged={this.state.isLogged}/>} />
+					{isLogged && <Route path="/useradmin" component={() => <UserAdmin isLogged={isLogged} />} />}
+				</Switch> */}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ front/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="ehpad-header mob-12 tab-12 dsk-12 mx-auto bgcolor1">
+            <Link to="/">
+                <img className="logo mob-6 tab-6 dsk-6" src="./logo.png" />
+            </Link>
+        </div>
+    )
+};
+
+export default Navbar;
